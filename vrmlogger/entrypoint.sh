21b3963dffb74f707d8e400b8ff5f261a188b6db
Gul Dukat <81039430+Dukat-Gul@users.noreply.github.com>
2023-06-05 12:08:16 +0800

    Update entrypoint.sh - Add DC System and update frequency to VRM portal
    Changes to the /data/conf/settings.xml which enable the DC system and change the default 900 second reporting to VRM portal to every 60 seconds.

--- vrmlogger/entrypoint.sh
+++ vrmlogger/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 mkdir -p /data/conf /data/db
+sed -i 's+>900</LogInterval>+>60</LogInterval>+g' /data/conf/settings.xml
+sed -i 's+>0</HasDcSystem>+>1</HasDcSystem>+g' /data/conf/settings.xml
+mkdir -p /data/home /data/home/vnctunnel
 
 echo "VRM portal ID: $(ip link ls dev eth0 | grep ether | awk '{print $2}' | sed 's/://g')"
 echo
