0d6cf44545bbc9884c6c9b83a2a598950e990de9
Gul Dukat <81039430+Dukat-Gul@users.noreply.github.com>
2023-06-06 13:11:55 +0800

    Update entrypoint.sh VRM remote MQTT
    Preliminary addition of Mqtt to settings.conf to allow VRM access via remote MQTT - Work In Progress.

--- vrmlogger/entrypoint.sh
+++ vrmlogger/entrypoint.sh
@@ -3,6 +3,11 @@
 mkdir -p /data/conf /data/db
 sed -i 's+>900</LogInterval>+>60</LogInterval>+g' /data/conf/settings.xml
 sed -i 's+>0</HasDcSystem>+>1</HasDcSystem>+g' /data/conf/settings.xml
+sed -i '/</Bol>/a \    \<MqttVrm type="i" min="0" max="1" default="0" silent="False">1</MqttVrm>' settings.xml
+sed -i '/</Bol>/a \    \<MqttN2k type="i" min="0" max="1" default="0" silent="False">0</MqttN2k>' settings.xml
+sed -i '/</Bol>/a \    \<MqttLocalInsecure type="i" min="0" max="1" default="0" silent="False">0</MqttLocalInsecure>' settings.xml
+sed -i '/</Bol>/a \    \<MqttLocal type="i" min="0" max="1" default="0" silent="False">0</MqttLocal>' settings.xml
+
 mkdir -p /data/home /data/home/vnctunnel
 
 echo "VRM portal ID: $(ip link ls dev eth0 | grep ether | awk '{print $2}' | sed 's/://g')"
